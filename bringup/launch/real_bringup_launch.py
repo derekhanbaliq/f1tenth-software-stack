7d0b8e4d1540a05baef899ba9fb3b49d4739e728
derekhanbaliq <derekzhou1105@gmail.com>
2023-04-20 19:48:45 -0400

    FINAL RACE FINAL

--- bringup/launch/real_bringup_launch.py
+++ bringup/launch/real_bringup_launch.py
@@ -63,7 +63,7 @@ def generate_launch_description():
         parameters=[
             {"sim or real": real},                          # do not change
             {"is ascending": True},                         # direction of waypoints (True for ccw)
-            {"csv name": "border07"},                      # csv for map used
+            {"csv name": "combined"},                      # csv for map used
             {"csv path": derek_csv_path},                   # path of csv for map in directory
             {"reference speed gain": 0.9},                  # weight of reference speed
             {"lookahead distance": 2.5},                    # lookahead of pure pursuit
